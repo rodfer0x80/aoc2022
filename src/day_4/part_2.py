08ada3bde7c6d62a772a8eb0131a4a866383383d
rodfer0x80 <rodfer0x80@proton.me>
2022-12-05 20:33:18 +0000

    day_4 part_1 and part_2

---
+++ src/day_4/part_2.py
@@ -0,0 +1,73 @@
+#!/usr/bin/env python3
+
+
+import sys
+import numpy as np
+
+
+def parse(data):
+    data_set = list()
+    for line in data:
+        p0, p1 = line.split(",")
+
+        a, b = p0.split("-")
+        a, b = int(a), int(b)
+
+        c, d = p1.split("-")
+        c, d = int(c), int(d)
+
+        xs = set()
+        for i in range(a, b+1):
+            xs.add(i)
+
+        ys = set()
+        for i in range(c, d+1):
+            ys.add(i)
+
+        data_set.append([xs, ys])
+
+    return data_set
+
+
+def readfile(sep="\n"):
+    try:
+        f = open("input.txt")
+        data = f.read().split(sep)
+        f.close()
+    except Exception as e:
+        sys.stderr.write(f"{e}\n")
+
+    for line in data:
+        if line == "":
+            data.remove(line)
+
+    return parse(data)
+
+
+def parseResult(res):
+    return res
+
+
+def solve(data_set):
+    res = 0
+    for p in data_set:
+        for x in p[0]:
+            if x in p[1]:
+                res += 1
+                break
+
+    return parseResult(res)
+
+
+def output(res):
+    sys.stdout.write(f"{res}\n")
+    return 0
+
+
+def main():
+    output(solve(readfile()))
+    return 0
+
+
+if __name__ == '__main__':
+    sys.exit(main())
